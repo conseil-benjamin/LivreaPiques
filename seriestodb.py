ddb0f53f4f46add78d6c4182eeff340275b0ea16
VenomSE30 <venomepitome@gmail.com>
2024-11-12 11:39:20 +0100

    I have a DataFrame/CSV file of all the series present in the book csv file

---
+++ seriestodb.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+data = pd.read_csv('new_data/books_corrected.csv')
+
+#get the "series" column of the dataframe
+
+series = data['series']
+
+#delete all the NaN values of series
+series = series.dropna()
+
+# Delete the parenthesis from the string
+
+s_no_parens = series.str.strip('()')
+
+# Split the string at ' #' and take the first part
+s_cleaned = s_no_parens.str.split(' #').str[0]
+
+#Select distinct values from s_cleaned
+distinct_values = s_cleaned.unique()
+
+#convert numpy array to dataframe
+distinct_values_df = pd.DataFrame(distinct_values)
+
+#save the dataframe to a csv file
+distinct_values_df.to_csv('new_data/SeriesList.csv', index=False)
+

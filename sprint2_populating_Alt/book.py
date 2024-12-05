0b1085c8633eb2ac87613ef1e2bdc734e842431c
StefanB35 <beaulieu.stefan.2004@gmail.com>
2024-12-05 12:01:04 +0100

    tentative rajout de livre

--- sprint2_populating_Alt/book.py
+++ sprint2_populating_Alt/book.py
@@ -13,12 +13,19 @@ filtered_data.rename(columns={'id': 'id_user'}, inplace=True)
 # Transformer les colonnes des livres en une seule colonne
 melted_data = filtered_data.melt(id_vars=['id_user'], 
                                  value_vars=['favorite_book_1', 'favorite_book_2', 'favorite_book_3'], 
-                                 var_name='book_rank', 
-                                 value_name='book_title')
+                                 var_name='rank', 
+                                 value_name='id')
 
-# Supprimer les lignes où la colonne 'book_title' est vide (NaN)
-melted_data.dropna(subset=['book_title'], inplace=True)
+# Remplacer les valeurs dans la colonne 'rank'
+melted_data['rank'] = melted_data['rank'].replace({
+    'favorite_book_1': 1,
+    'favorite_book_2': 2,
+    'favorite_book_3': 3
+})
+
+# Supprimer les lignes où la colonne 'id' est vide (NaN)
+melted_data.dropna(subset=['id'], inplace=True)
 
 # Enregistrer dans un nouveau fichier CSV
-output_file = 'C:/Users/utilisateur/Documents/Perso/SAE/LivreaPiques/new_data/book_fav_flattened.csv'
+output_file = 'C:/Users/utilisateur/Documents/Perso/SAE/LivreaPiques/new_data/book_fav.csv'
 melted_data.to_csv(output_file, index=False)

f3ec5157abc7b929400933ee1cb8ec61a850b928
esdebroise <esteban.debroise@etudiant.univ-rennes1.fr>
2025-02-03 16:42:08 +0000

    ajout d'interactivité

--- application_system_reco/main.py
+++ application_system_reco/main.py
@@ -3,9 +3,16 @@ from system_reco.reco_esteban import *
 from system_reco.reco_benjamin import *
 
 def main():
-    print(reco_esteban(1))
-    reco_benj = FinalRecommender()
-    print(reco_benj.get_recommendations(1, 5))
+    #demander si on veut la 1er reco
+    user_id = int(input("Entrez l'id de l'utilisateur: "))
+    if input("Voulez-vous la première recommandation ? (Y/N)") == "Y":
+        print(reco_esteban(user_id))
+
+    if input("Voulez-vous la deuxième recommandation ? (Y/N)") == "Y":
+        reco_benj = FinalRecommender()
+        print(reco_benj.get_recommendations(1, 5))
+    
+    
 
 if __name__ == '__main__':
     main()

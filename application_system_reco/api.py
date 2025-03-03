5e7160452722acabf7df33e8e7541dc3751f9aab
VenomSE30 <venomepitome@gmail.com>
2025-03-03 13:45:28 +0100

    I resolved the 422 Error

--- application_system_reco/api.py
+++ application_system_reco/api.py
@@ -16,66 +16,58 @@ async def http_exception_handler(request: fa.Request, exc: fa.HTTPException):
 async def read_root():
     return {"message": "Welcome to the Big Book Society API"}
 
+@app.get("/api/books/title={book_title}")
+async def read_api(title: str):
+    print(title)
+    title = title.replace("'", "''")
+    df = requete("""    SELECT *
+                        FROM allbookdata
+                        WHERE TRIM(allbookdata.book_title) ILIKE '{title}'""".format(title=title))  
+    if df.empty:
+        return fa.Response(status_code=204)
+    df = df.fillna(value=False)
+    print("Where does it bugs ?")
+    return df.to_dict(orient="records")
+
 @app.get("/api/books")
 async def read_api():
     df = requete("""    SELECT * 
-                        FROM book 
-                        JOIN book_author ON book.book_id = book_author.book_id
-                        JOIN author ON book_author.author_id = author.author_id
-                        UNION
-                        SELECT *
-                        FROM book
-                        LEFT JOIN book_author ON book.book_id = book_author.book_id
-                        LEFT JOIN author ON book_author.author_id = author.author_id""")
+                        FROM allbookdata""")
     if df.empty:
         return fa.Response(status_code=204)
     df = df.fillna(value=False)
     return df.to_dict(orient="records")
 
-@app.get("/api/books/name={book_title}")
-async def read_api(book_title: str):
-    print(book_title)
-    book_title = book_title.replace("'", "''")
+@app.get("/api/books/name={author_name}")
+async def read_api(author_name: str):
+    print(author_name)
+    author_name = author_name.replace("'", "''")
     df = requete("""    SELECT * 
-                        FROM book 
-                        JOIN book_author ON book.book_id = book_author.book_id
-                        JOIN author ON book_author.author_id = author.author_id
-                        WHERE TRIM(book.book_title) ILIKE '{title}'
-                        UNION
-                        SELECT *
-                        FROM book
-                        LEFT JOIN book_author ON book.book_id = book_author.book_id
-                        LEFT JOIN author ON book_author.author_id = author.author_id
-                        WHERE TRIM(book.book_title) ILIKE '{title}'""".format(title=book_title))  
+                        FROM allbookdata
+                        WHERE TRIM(allbookdata.authors) ILIKE '{author_name}'""".format(author_name=author_name))
     if df.empty:
         return fa.Response(status_code=204)  
     df = df.fillna(value=False)
     return df.to_dict(orient="records")
 
-@app.get("/api/books/id={book_id}")
-async def read_api(book_id: int):
-    df = requete("""    SELECT * 
-                        FROM book 
-                        JOIN book_author ON book.book_id = book_author.book_id
-                        JOIN author ON book_author.author_id = author.author_id
-                        WHERE book.book_id = {id}
-                        UNION
-                        SELECT *
-                        FROM book
-                        LEFT JOIN book_author ON book.book_id = book_author.book_id
-                        LEFT JOIN author ON book_author.author_id = author.author_id
-                        WHERE book.book_id = {id}""".format(id=book_id))    
+@app.get("/api/books/genre={genre}")
+async def read_api(genre: str):
+    print(genre)
+    genre = genre.replace("'", "''")
+    genre = "%" + genre + "%"
+    df = requete("""    SELECT *
+                        FROM allbookdata
+                        WHERE TRIM(allbookdata.genres) ILIKE '{genre}'""".format(genre=genre))
     if df.empty:
         return fa.Response(status_code=204)
     df = df.fillna(value=False)
     return df.to_dict(orient="records")
 
-@app.get("/api/author/{author_name}")
-async def read_api(author: str):
-    author = author.replace("'", "''")
-    df = requete("""    SELECT * 
-                        FROM author
-                        WHERE TRIM(author_name) ILIKE '{author_name}'""".format(author_name=author))
+@app.get("/api/books/id={book_id}")
+async def read_api(book_id: int):
+    df = requete("""    SELECT *
+                        FROM allbookdata
+                        WHERE allbookdata.book_id = {id}""".format(id=book_id))    
     if df.empty:
         return fa.Response(status_code=204)
     df = df.fillna(value=False)
